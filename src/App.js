018460ddf39051eb393e2b39e828a3b1990717f3
Patrick Walker <41069751+SouthUniform7@users.noreply.github.com>
2022-02-02 00:56:14 -0500

    added json server, about page, routing

--- src/App.js
+++ src/App.js
@@ -1,49 +1,88 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
 
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {//this is static data. It wont be changed by any functions.
-      //if we wanted to change this we'd make fetch or HTTP requests to a server for this
-        id: 1,
-        text: 'Homework',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Videogames',
-        day: 'Feb 5th at 2:31pm',
-        reminder: false,
-    },
-    {
-        id: 3,
-        text: 'Legos',
-        day: 'Feb 5th at 2:31:15pm',
-        reminder: true,
-    },
-])
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }    
+
+    getTasks()
+  }, [])
+
+    //Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
+    //Fetch Task
+    const fetchTask = async (id) => {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`)
+      const data = await res.json()
+  
+      return data
+    }
 
 //Add task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() *1000) +1
+const addTask = async (task) => {
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST', 
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+
+  const data = await res.json()
+
+  setTasks([...tasks, data])
+
+
+  /* const id = Math.floor(Math.random() *1000) +1
   const newTask = { id, ...task}
-  setTasks([...tasks, newTask])
+  setTasks([...tasks, newTask]) */
 }
 
 //Delete Task
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+
+  await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'DELETE' 
+  })
+
   //console.log('delete', id)
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
 //Toggle reminder
 
-const toggleReminder = (id) => {
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+
   setTasks(
     tasks.map((task) => 
     task.id === id ? {...task, reminder: !task.reminder} : task
@@ -64,23 +103,34 @@ const showFile = (e) => {
 }; 
 */
 
+
+//uses Router v6
   return (
+    <Router>
     <div className='container'>
       <Header  
       onAdd={() => setShowAddTask(!showAddTask)}
       showAdd={showAddTask}
        />
 
-      {showAddTask && <AddTask onAdd={addTask}/>}
+      <Routes>
+      <Route path='/' exact element={
+          <>
+          {showAddTask && <AddTask onAdd={addTask}/>}
 
+        
+          {tasks.length >0 ? (
+          <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+          ) : ('No Tasks to Show')
+          }
+          </>
+      }/>
       
-      {tasks.length >0 ? (
-      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : ('No Tasks to Show')
-      }
-      
+      <Route path='/about' element={<About/>}/>
+      </Routes>
+      <Footer />
     </div>
-    //only deletes items from the UI, items are not stored on a centralized server. That can be done with a json server possibly
+    </Router>
 
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
+import { useLocation } from 'react-router-dom'
 
 const Header = ({ title, onAdd, showAdd}) => {
 
+    const location = useLocation()
 
   return <header className='header'>
       <h1>{title}</h1>
+      {location.pathname === '/' && (
       <Button 
       color={showAdd ? '#125254' : '#a6dab4'}
       textColor={showAdd ? '#a6dab4' : '#125254'}
       text={showAdd ? 'This is where the fun begins' : 'Hello There'} 
       onClick={onAdd} />
+      )}
       
   </header>;
 }
